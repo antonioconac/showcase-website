c1aa126d078e2c174f10f5c95ca2a1c7cac644ff
antonioconac <conacheantonio13@gmail.com>
2025-07-29 16:22:45 +0300

    Updated the projects list + added dependencies through 'requirements.txt'

--- Home.py
+++ Home.py
@@ -38,14 +38,14 @@ col3, col5, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv("data.csv", sep=",")
 
 with col3:
-    for index, row in df[:2].iterrows():
+    for index, row in df[:3].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"{row['url']}")
 
 with col4:
-    for index, row in df[2:4].iterrows():
+    for index, row in df[3:5].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
