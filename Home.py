e9525d84bb75375659282b016eea90e3f547b1b2
antonioconac <conacheantonio13@gmail.com>
2025-08-07 15:21:59 +0300

    Updated the projects list

--- Home.py
+++ Home.py
@@ -38,14 +38,14 @@ col3, col5, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv("data.csv", sep=",")
 
 with col3:
-    for index, row in df[:3].iterrows():
+    for index, row in df[:4].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"{row['url']}")
 
 with col4:
-    for index, row in df[3:5].iterrows():
+    for index, row in df[4:8].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
